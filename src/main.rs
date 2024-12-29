323003fcc07ca4f6d73cb6e0b5ec4c23e7995a7b
Steve Biedermann <hardliner66@gmail.com>
2024-12-29 15:43:48 +0100

    add logic opcodes

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ enum Action {
 }
 
 fn run(memory: &[u32], verbose: bool) -> anyhow::Result<()> {
-    let mut cpu = NativeCpu::new(1024 * 1024, 6, NullHostIO);
+    let mut cpu = NativeCpu::new(1024 * 1024, 16, NullHostIO);
     cpu.set_verbose(verbose);
 
     cpu.load_memory(0, memory);
